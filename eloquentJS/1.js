d5c698fdc63de1e27a533b8354acca78486a3120
fdillanes <fdillanes@gmail.com>
2016-02-28 02:21:26 -0300

    seguimos ejercitando

---
+++ eloquentJS/1.js
@@ -0,0 +1,17 @@
+/* 
+Looping a triangle: 
+Write a loop that makes seven calls to console.log to output the 
+following triangle
+#
+##
+###
+####
+...
+*/
+
+var a = "";
+for (i = 0; i < 8; i++){
+	console.log(a);
+	a = a + "#";
+	}
+
