af0eb3a4f55a67a1d8007e35512c008663debbc3
fdillanes <fdillanes@gmail.com>
2016-02-28 12:59:11 -0300

    first commit

---
+++ eloquentJS/chessboard.js
@@ -0,0 +1,22 @@
+/*
+Chess board
+
+Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
+*/
+var blanca = " ";
+var negra = "#";
+var linea = "";
+for (t = 0; t < 4; t++){
+    for (i = 0; i < 8; i++){
+        linea = linea + blanca;
+        linea = linea + negra;
+    }
+    console.log(linea, "\n");
+    linea = "";
+    for (j = 0; j < 8; j++){
+        linea = linea + negra;
+        linea = linea + blanca;
+    }
+    console.log(linea, "\n");
+    linea = "";
+}
